712371cb476a3b5e065c01c90830f70e1791e271
kuhanis <kuhanis380@gmail.com>
2025-01-04 18:14:35 +0800

    csrf

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,6 +6,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <title>@yield('title', 'Login') | Nadimah Tailor</title>
 <link rel="apple-touch-icon" href="{{asset('app-assets/images/ico/apple-icon-120.png')}}">
